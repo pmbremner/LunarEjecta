e8db4a736669df34ccf4a9e89d9dcd71c6e5f50b
amdestefano <61249904+anthony-destefano@users.noreply.github.com>
2020-03-09 12:02:07 -0500

    Update ephemeris_parser.py

--- ephemeris_parser.py
+++ ephemeris_parser.py
@@ -1,3 +1,36 @@
+# Used to parse ephemeris files downloaded from https://ssd.jpl.nasa.gov/horizons.cgi#top
+#  with the following inputs (XX is degrees latitude, Y is (N)orth or (S)outh):
+## Ephemeris Type [change] : 	VECTORS
+## Target Body [change] : 	Moon [Luna] [301]
+## Coordinate Origin [change] : 	Topocentric @301 [Moon] ( 0°00'00.0''E, XX°00'00.0''Y, 0.6 km )
+## Time Span [change] : 	Start=2020-01-01, Stop=2039-01-01, Intervals=10000
+## Table Settings [change] : 	output units=KM-S; labels=NO
+## Display/Output [change] : 	plain text
+#
+# These inputs generate an email script of:
+#  which can be emailed to horizons@ssd.jpl.nasa.gov with the subject line of "JOB":
+## !$$SOF
+## COMMAND= '301'
+## CENTER= 'coord@301'
+## COORD_TYPE= 'GEODETIC'
+## SITE_COORD= '0,XX,0.6'
+## MAKE_EPHEM= 'YES'
+## TABLE_TYPE= 'VECTORS'
+## START_TIME= '2020-01-01'
+## STOP_TIME= '2039-01-01'
+## STEP_SIZE= '10000'
+## OUT_UNITS= 'KM-S'
+## REF_PLANE= 'ECLIPTIC'
+## REF_SYSTEM= 'J2000'
+## VECT_CORR= 'NONE'
+## VEC_LABELS= 'NO'
+## VEC_DELTA_T= 'NO'
+## CSV_FORMAT= 'NO'
+## OBJ_DATA= 'YES'
+## VEC_TABLE= '3'
+## !$$EOF
+
+
 import glob
 
 offset = 58
