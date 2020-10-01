016de626a4916fd2ffe0222f7b029c1124414173
amdestefano <61249904+anthony-destefano@users.noreply.github.com>
2020-10-01 11:24:45 -0500

    Update 10/1/20

--- LaTeX_Report/Plot_Zook67_Fig2.py
+++ LaTeX_Report/Plot_Zook67_Fig2.py
@@ -12,8 +12,13 @@ def beta(a, m, n):
 def gamma(a, m, n):
 	return -1. + a/3.*(1. - 3.*m / (2.*n))
 
+def beta2(a, g, n):
+	return -n/g * (3./a * (1.+g) - 1.)
+
 def gamma2(a, b, n):
 	return -(3. - a) / (3. - a*b/n)
+
+
 
 
 filename = "Zook1967_Fig2.txt" #sys.argv[1]
@@ -43,6 +48,9 @@ a = np.linspace(0.3, 0.7, N).reshape(1, N)
 beta_Ve  = beta(a, 0.4, np.repeat(dG_dVe_c.reshape(np.size(dG_dVe_c), 1), N, axis=1)/2.)
 gamma_Ve = gamma2(a, np.average(beta_Ve, axis=0), np.repeat(dG_dVe_c.reshape(np.size(dG_dVe_c), 1), N, axis=1)/2.)
 
+
+beta_Ve2  = beta2(a, -1.25, np.repeat(dG_dVe_c.reshape(np.size(dG_dVe_c), 1), N, axis=1)/2.)
+
 #print(np.shape(Ve.reshape(1, np.size(Ve) )))
 
 plt.plot(Ve, G)
@@ -51,12 +59,16 @@ plt.figure()
 plt.plot(Ve[:-1], dG_dVe)
 plt.plot(Ve, dG_dVe_c)
 
+print(np.average(dG_dVe_c[:-1], weights=dVe))
+
 plt.figure()
-plt.plot(Ve, beta_Ve)
-plt.plot(Ve, -gamma_Ve)
+#plt.plot(Ve, beta_Ve)
+plt.plot(Ve, beta_Ve2)
+#plt.plot(Ve, -gamma_Ve)
 
 plt.figure()
 #plt.plot(Ve, gamma_Ve )
 plt.plot(a.T, np.average(beta_Ve, axis=0) )
-plt.plot(a.T, np.average(-gamma_Ve, axis=0) )
+plt.plot(a.T, np.average(beta_Ve2, axis=0) )
+#plt.plot(a.T, np.average(-gamma_Ve, axis=0) )
 plt.show()
