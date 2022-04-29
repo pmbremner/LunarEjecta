5a4d719c6b6148917dab6d650f9b3fd1c91b6db4
amdestefano <61249904+anthony-destefano@users.noreply.github.com>
2022-04-28 23:52:41 -0500

    latex update night 4-28-22

--- LaTeX_ReportV2/plot_distance_dist2.py
+++ LaTeX_ReportV2/plot_distance_dist2.py
@@ -21,11 +21,14 @@ def d_dist(v, g, rs):
 	Fi = F(v,g,rs)
 	return np.arctan2(2.*v**2 * np.sin(g)*np.cos(g) + ((rs-1.)/(1.-Fi))*(2.*v**2-1.)*np.tan(g), ((rs-Fi)/(1.-Fi))-2.*(v*np.sin(g))**2 )
 
+def v_min(g, rs):
+	return np.sqrt((2.*rs*(rs-1.)) / (np.cos(2.*g)-1. + 2.*rs**2))
+
 rs = float(sys.argv[1])
+d  = float(sys.argv[2])
 
-
-g = np.linspace(0., np.pi/2, 500)
-v = np.linspace(0, 1., 500)
+g = np.linspace(0.0001, np.pi/2*0.9999, 500)
+v = np.linspace(0.0001, 1., 500)
 G, V = np.meshgrid(g, v)
 
 
@@ -33,7 +36,12 @@ D = d_dist(V, G, rs)
 
 #D0 = d_dist(V, G, 1.)
 
-plt.pcolormesh(G, V, D, cmap='jet',shading='gouraud') #gist_ncar
+plt.pcolormesh(G, V, D, cmap='prism',shading='gouraud') #gist_ncar, nipy_spectral, gist_rainbow, plasma
 
+plt.plot(g, v_min(g, rs), color='black', linewidth=4, linestyle='--')
+
+plt.plot(g, v_speed(d, g, rs), color='black', linewidth=3)
+
+plt.ylim(0,1)
 
 plt.show()
