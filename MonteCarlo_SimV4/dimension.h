c5f03498babc18fcebcf29d12a6714d55e31851f
amdestefano <61249904+anthony-destefano@users.noreply.github.com>
2022-09-21 09:18:03 -0500

    Update morning 9/21/22

--- MonteCarlo_SimV4/dimension.h
+++ MonteCarlo_SimV4/dimension.h
@@ -1,6 +1,24 @@
 #ifndef DIMENSION_H
 #define DIMENSION_H
 
+using namespace std;
+
+// virtual class, must create derived classes
+class dimension
+{
+public:
+	dimension();
+	~dimension();
+
+	// pure virtual function, derived class must implement this function
+	virtual void getSample() = 0;
+
+protected:
+	vector<attribute*> element;
+
+	vector<dim*> childDim;
+	
+};
 
 
 
